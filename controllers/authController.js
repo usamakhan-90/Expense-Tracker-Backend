8d31516ff4d32c6c456938500a6a672c89c5c9d8
Usama Khan <us.usamashahid@gmail.com>
2025-07-17 23:26:53 +0500

    Implement user login, logout, and profile retrieval functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,10 @@
 const userModel = require("../models/userModel");
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const cloudianry = require('cloudinary')
 const bcrypt = require('bcrypt');
 const getDataUri = require("../utils/features");
+
+
 const registerUser = async(req, res)=>{
     try {
         const {fullname, email, password} = req.body;
@@ -61,4 +63,110 @@ const registerUser = async(req, res)=>{
     }
 }
 
-module.exports = {registerUser}
+const loginUser = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "Please provide the email and password"
+            })
+        }
+
+        let user = await userModel.findOne({email});
+
+        if(!user)
+        {
+            return res.status(404).json({
+                success: false,
+                message: "User is not registered is yet for this email"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch)
+        {
+            return res.status(404).json({
+                success: false,
+                message: "Passowrd is incorrect please enter the correct password"
+            })
+        }
+
+        const token = jwt.sign({
+            id: user._id
+        }, process.env.SECRET_KEY, {
+            expiresIn: '1h'
+        });
+
+        return res.status(200).cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: true,
+            expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 100)
+        }).json({
+            success: true,
+            message: "User is login successfully",
+            token,
+            user
+        })  
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in login API",
+            error
+        })
+    }
+}
+
+const logoutUser = async(req, res)=>{
+    try {
+        return res.status(200).cookie("token", "", {
+            httpOnly: true,
+            secure: true,
+            sameSite: true,
+            expires: new Date(Date.now())
+        }).send({
+            success: true,
+            message: "User is logout successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in Logout Api",
+            error
+        })
+    }
+}
+
+const getUser = async(req, res)=>{
+    try {
+        const user = await userModel.findById(req.user.id).select('-password');
+
+        if(!user)
+        {
+            return res.status(404).json({
+                success: false,
+                message: "User is not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "user info is fetched successfully",
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in Get Info API",
+            error
+        })
+    }
+}
+module.exports = {registerUser, loginUser, logoutUser, getUser}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,15 @@
 var express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, getUser } = require('../controllers/authController');
 const singleUpload = require('../middlewares/multer');
+const isAuth = require('../middlewares/authMiddleware');
 
 var router = express.Router();
 
 router.post("/register", singleUpload, registerUser )
 
+router.post("/login", loginUser );
+
+router.post("/logout", isAuth, logoutUser);
+
+router.get("/profile", isAuth, getUser);
 module.exports = router;
